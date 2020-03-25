fde051588f8f0da03569ea202a771a5b830b72f1
Colin Jaffe <balloonasaurus@gmail.com>
2020-03-24 20:41:51 -0400

    Fixes typo.

--- main.test.js
+++ main.test.js
@@ -96,7 +96,7 @@ describe('makeQuestion', () => {
 })
 
 describe('questionings', () => {
-  it(`adds an exclamation point to the end of each string`, () => {
+  it(`adds a question mark to the end of each string`, () => {
     expect(questionings(['hello', 'there', 'you absolute fiend'])).toEqual(['hello?', 'there?', 'you absolute fiend?']);
     expect(questionings(['hey', 'you'])).toEqual(['hey?', 'you?']);
   })
